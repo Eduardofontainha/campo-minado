2d75f27f91c3d8485c3b767ef71b969189bb32b2
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-07-31 17:57:07 -0300

    :sparkles:feat: Atualiza lógica do jogo para seleção de dificuldade e reinício, além de melhorias no controle mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,72 @@
-import "./componentes/Tabela.js";
-import "./componentes/Quadrado.js";
-import { CampoMinado } from "./logica/Jogo.js";
+import "./components/Tabela.js";
+import "./components/Quadrado.js";
+import { CampoMinado } from "./logic/Jogo.js";
 
-const jogo = new CampoMinado(10, 10);
 const tabela = document.querySelector("layout-campo");
+const reiniciarBtn = document.getElementById("btn-reiniciar");
+const selecionarDificuldade = document.getElementById("selecionar-dificuldade");
 
-tabela.iniciar(jogo, 20);
+let quantidadeMinasAtual = 10; 
+const jogo = new CampoMinado(10, quantidadeMinasAtual);
+
+const executarReiniciarJogo = () => {
+    tabela.reiniciarJogo();
+    tabela.iniciar(jogo, quantidadeMinasAtual);
+};
+
+if (selecionarDificuldade) {
+    selecionarDificuldade.addEventListener("change", (event) => {
+        const dificuldade = event.target.value;
+        switch (dificuldade) {
+            case "facil":
+                quantidadeMinasAtual = 10;
+                break;
+            case "medio":
+                quantidadeMinasAtual = 20;
+                break;
+            case "dificil":
+                quantidadeMinasAtual = 30;
+                break;
+            default:
+                quantidadeMinasAtual = 20;
+        }   
+        executarReiniciarJogo();
+    });
+}
+
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener("click", () => {
+        executarReiniciarJogo();
+    });
+}
+
+const btnModalJogar = document.getElementById("btn-modal-jogar");
+if (btnModalJogar) {
+    btnModalJogar.addEventListener("click", () => {
+        const modal = document.getElementById("modal-fim-jogo");
+        if (modal) modal.classList.remove("ativo"); 
+        executarReiniciarJogo(); 
+    });
+}
+
+tabela.iniciar(jogo, quantidadeMinasAtual);
+
+//Controle Mobile
+const btnModoToque = document.getElementById("btn-modo-toque");
+window.modoMobileBandeira = false;
+
+if (btnModoToque) {
+    btnModoToque.addEventListener("click", () => {
+        window.modoMobileBandeira = !window.modoMobileBandeira;
+
+        if (window.modoMobileBandeira) {
+            btnModoToque.classList.remove("modo-cavar");
+            btnModoToque.classList.add("modo-bandeira");
+            btnModoToque.innerHTML = '<span class="icone-modo">🚩</span> Modo: Bandeira';
+        } else {
+            btnModoToque.classList.remove("modo-bandeira");
+            btnModoToque.classList.add("modo-cavar");
+            btnModoToque.innerHTML = '<span class="icone-modo">⛏️</span> Modo: Revelar';
+        }
+    });
+}
